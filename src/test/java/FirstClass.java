1d2fe23c61c58355bb9a2a18dcb4cf4ef62017dd
Andemicael Miriam <miriamyemane@gmail.com>
2019-11-21 17:07:55 -0500

    test

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,12 +3,7 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("Hi World");
-<<<<<<< HEAD
-        System.out.println("I AM WORKING");
-        System.out.println("I AM WORKING");
-=======
         System.out.println("Hello world until 7");
 
->>>>>>> 725d382ed0f09276d0da921eea04e3ef2354ba10
     }
 }
