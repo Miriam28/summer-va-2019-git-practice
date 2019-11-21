b623824c7447df5c8b8de42daee490b881ffdd67
Andemicael Miriam <miriamyemane@gmail.com>
2019-11-21 15:32:29 -0500

    initialcommit

---
+++ src/test/java/GitNote.java
@@ -0,0 +1,14 @@
+public class GitNote {
+
+    /*
+
+
+
+
+create a git ignore file: we donnot use this alot
+    gitignore tells git which files should be ignored by git. reports, target folder,
+    same output folder should be ignore. They should not  be tracked by git
+
+     1,
+     */
+}
